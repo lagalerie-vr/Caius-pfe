5dc4e1800868130df3699410ef66d9b5bd977d46
mohamed l'wed <mohamed.elwed@esen.tn>
2022-04-26 21:39:56 +0100

    Update AuthProvider.jsx

--- client/src/contexts/AuthProvider.jsx
+++ client/src/contexts/AuthProvider.jsx
@@ -1,18 +1,8 @@
-import Get from "../data/Functions/Get";
-
-export function parseJwt(token) {
-    if (!token) { return; }
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
-    return JSON.parse(window.atob(base64));
-}
-
-
 export const user = {
     name: 'mimi lousif',
     email: 'tom@example.com',
     telephone: '98989898',
     imageUrl:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    role: ""
+    role: "Admin"
 }
