24b7e174fd0b13c717d6224a7ca49b0c0c5aadfa
mohamed l'wed <mohamed.elwed@esen.tn>
2022-05-03 09:03:24 +0100

    n

--- client/src/Components/DashboardAdmin/Main.jsx
+++ client/src/Components/DashboardAdmin/Main.jsx
@@ -1,13 +1,14 @@
-
+import Pie from '../Stats/PieChart'
 
 function Main() {
+
     return (
 
         <>
             <section className="text-gray-600 body-font">
 
                 <div className="flex flex-wrap -m-4 text-center">
-
+                    <Pie />
                 </div>
 
             </section>
