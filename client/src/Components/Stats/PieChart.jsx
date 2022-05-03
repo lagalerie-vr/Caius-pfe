0f22704cf77c04da7f2b734acd94e2745f63b115
mohamed l'wed <mohamed.elwed@esen.tn>
2022-05-03 12:43:21 +0100

    v

--- client/src/Components/Stats/PieChart.jsx
+++ client/src/Components/Stats/PieChart.jsx
@@ -1,21 +1,59 @@
-/* This example requires Tailwind CSS v2.0+ */
-const stats = [
-    { name: 'Total Subscribers', stat: '71,897' },
-    { name: 'Avg. Open Rate', stat: '58.16%' },
-    { name: 'Avg. Click Rate', stat: '24.57%' },
-]
+import { Link } from 'react-router-dom'
+import { CursorClickIcon, MailOpenIcon, UsersIcon } from '@heroicons/react/outline'
+import useGet from '../../data/Functions/useGet'
+
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
 
 export default function Example() {
+    const Clients = useGet("/users/role/Client")
+    const defaults = useGet("/users/role/default")
+    const Creation = useGet("/creations/state/En Cours de traitement")
+    const Domicilation = useGet("/domiciliation/state/En Cours de traitement")
+    const expert = useGet("/creations/state/Passer a l'Expert")
+    const expert2 = useGet("/domiciliation/state/Passer a l'Expert")
+    const Refuser = useGet("/domiciliation/state/Refuser")
+    const Refuser2 = useGet("/creations/state/Refuser")
+
+    const stats = [
+        { id: 1, name: 'N° Client', stat: Clients.length + defaults.length, icon: UsersIcon, link: "/Users" },
+        { id: 2, name: 'N° Demande en attente', stat: Creation.length + Domicilation.length, icon: MailOpenIcon, link: "/demande" },
+        { id: 3, name: "N° Demande passer a l'expert", stat: expert.length + expert2.length, icon: CursorClickIcon, link: "/demande" },
+        { id: 3, name: "N° Demande des demande refuser par l'expert", stat: Refuser2.length + Refuser.length, icon: CursorClickIcon, link: "/demande" },
+    ]
+
     return (
         <div>
-            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-2">
                 {stats.map((item) => (
-                    <div key={item.name} className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-                        <dt className="text-sm font-medium text-gray-500 truncate">{item.name}</dt>
-                        <dd className="mt-1 text-3xl font-semibold text-gray-900">{item.stat}</dd>
+                    <div
+                        key={item.id}
+                        className="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden"
+                    >
+                        <dt>
+                            <div className="absolute bg-indigo-800 rounded-md p-3">
+                                <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                            </div>
+                            <p className="ml-16 text-sm font-medium text-gray-500 truncate">{item.name}</p>
+                        </dt>
+                        <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
+                            <p className="text-2xl font-semibold text-gray-900">{item.stat}</p>
+                            <div className="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
+                                <div className="text-sm">
+                                    <Link to={item.link} className="font-medium text-indigo-800 hover:text-indigo-500">
+                                        Voir plus
+                                    </Link>
+                                </div>
+                            </div>
+                        </dd>
                     </div>
                 ))}
             </dl>
+            <hr className='mt-4 mb*4' />
         </div>
     )
 }
--- client/src/Pages/DashboardAdmin/Main.jsx
+++ client/src/Pages/DashboardAdmin/Main.jsx
@@ -1,10 +1,13 @@
 
 import Page from '../../Layouts/LayoutPage'
 import Stats from '../../Components/DashboardAdmin/Main'
+import { useUser } from '../../contexts/AuthProvider'
 export default function Main() {
+    const user = useUser()
     return (
+
         <Page
-            title="Dashboard"
+            title={`Bonjour, ${user.nom} !`}
             content={<Stats />}
         />
     )
