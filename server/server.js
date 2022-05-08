465eaaa9f9ead0c31ddbedfbce9a31e7ef8c2ff5
mohamed l'wed <mohamed.elwed@esen.tn>
2022-05-08 10:23:58 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require('cors')
 
+
 const userRoutes = require("./routes/userRoutes")
 const videoRoutes = require("./routes/videoRoutes")
 const creationRoutes = require("./routes/creationRoutes")
