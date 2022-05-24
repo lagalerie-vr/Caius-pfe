fb99704e14a2b578f9cd0b0e4eee2c24159eb50b
mohamed l'wed <mohamed.elwed@esen.tn>
2022-05-24 14:02:37 +0100

    s

--- client/src/Components/DashboardAdmin/Facture.jsx
+++ client/src/Components/DashboardAdmin/Facture.jsx
@@ -59,18 +59,15 @@ function FactureList() {
                                             >
                                                 Nom Gérant
                                             </th>
-
                                             <th
                                                 scope="col"
                                                 className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                                             >
                                                 Local
                                             </th>
-
                                             <th scope="col" className="relative px-6 py-3">
                                                 <span className="sr-only">Edit</span>
                                             </th>
-
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200">
